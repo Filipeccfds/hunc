e696f51cd6235ed3e249db51c4e0f6f76526fa4e
Filipeccfds <filipeccds@outlook.com>
2023-04-30 00:07:13 -0300

    meu primeiro module

---
+++ huncoding/go.mod
@@ -0,0 +1,3 @@
+module github.com/Filipeccfds/hunc
+
+go 1.20
---
+++ huncoding/hello.go
@@ -0,0 +1,9 @@
+// package hunc - pacote de tesete de modules
+package hunc
+
+import "fmt"
+
+//printhello  vai te dizer oi dentro module hunc
+func PrintHello() {
+	fmt.Println("helllo esse e um exemplo para se usar um module")
+}
